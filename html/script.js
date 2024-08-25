6f14e3e39ea575367687439183bcca5883aaba32
poonampaliwal12 <poonam.paliwal173@gmail.com>
2024-08-25 09:12:11 +0530

    first commit

---
+++ html/script.js
@@ -0,0 +1,4 @@
+// JavaScript to display an alert when the button is clicked
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Button clicked! Welcome to MyBrand!');
+});
